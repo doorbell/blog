0e178e00312b551b1741d936d35ab5359b008a7c
manavo <manavo@gmail.com>
2017-10-06 01:13:13 +0100

    Publishing to gh-pages (publish.sh)

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -7,7 +7,7 @@ DIR=$(dirname "$0")
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
+#    exit 1;
 fi
 
 echo "Deleting old publication"
@@ -17,7 +17,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
